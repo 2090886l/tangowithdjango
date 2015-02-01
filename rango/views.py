b9bc49cd04065745076a4c35e269f3e7ea3f2699
2090886l <ivailolafchiev@yahoo.com>
2015-02-01 19:14:48 +0000

    Chapter 10 - Completed

--- rango/views.py
+++ rango/views.py
@@ -207,4 +207,4 @@ def category(request, category_name_slug):
     
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
